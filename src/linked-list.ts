8d40ff4fa0b17e225acc0a0287098a9c996ca937
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-06-17 03:55:14 -0300

    added: data structured methods

--- src/linked-list.ts
+++ src/linked-list.ts
@@ -21,6 +21,53 @@ class LinkedList<T> {
     this.length++;
   }
 
+  insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let index = 0;
+      let previous = current;
+      let node = new ListNode(value);
+
+      if (pos === 0) {
+        node.next = this.root;
+        this.root = node;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeAt(pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let previous: ListNode<T> = current;
+      let index = 0;
+
+      if (pos === 0) {
+        this.root = current.next;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.length--;
+      return current;
+    } else {
+      return null;
+    }
+  }
+
   getNumberOfElements() {
     return this.length;
   }
@@ -36,12 +83,21 @@ class LinkedList<T> {
 
 const numberList = new LinkedList<number>();
 
-numberList.add(7);
-numberList.add(-3);
 numberList.add(10);
+numberList.add(5);
+numberList.add(-3);
 
-console.log(numberList.getNumberOfElements());
+console.log('Length: ' + numberList.getNumberOfElements());
 numberList.print();
 
-const list = new LinkedList<number>();
+console.log('--- NOW REMOVING INDEX 1 ---');
+numberList.removeAt(1);
+console.log('Length: ' + numberList.getNumberOfElements());
+numberList.print();
+
+console.log('--- NOW INSERTING AT INDEX 1 ---');
+numberList.insertAt(100, 1);
+console.log('Length: ' + numberList.getNumberOfElements());
+numberList.print();
+
 const nameList = new LinkedList<string>();
